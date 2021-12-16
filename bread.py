9dae2121cfe5e636e372fafa2a2ad6c7f61976ed
Shmikasa <96242109+Shmikasa@users.noreply.github.com>
2021-12-16 14:09:52 +0100

    Whats your favourit kind of bread?
    This Programm'll give you a good opinion about your favorite kind of bread.

---
+++ bread.py
@@ -0,0 +1,8 @@
+Bread = ["white bread","Toast","corny bread","dark bread","fluffy bread","soft bread","watery bread","old bread","croissant"]
+print(Bread)
+Question = "favorite bread:"
+Answer = str(input(Question))
+if Answer == Bread[0-8]:
+    print("You know your bread.")
+else:
+     print("I never heard of that kind of bread.")
